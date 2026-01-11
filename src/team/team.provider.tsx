7ad4943bb7f74fae6da12187bc19ce9e52717fe3
Tiago Santos <santos.t.j.dos@gmail.com>
2026-01-11 16:47:38 -0300

    fix: require authentication to load teams

--- src/team/team.provider.tsx
+++ src/team/team.provider.tsx
@@ -3,18 +3,22 @@ import { TeamContext } from './team.context';
 import type { Team } from './team.model';
 import service from './team.service';
 import { useTranslation } from 'react-i18next';
+import { useAuth } from '@/auth';
 
 type TeamProviderProps = {
   children: React.ReactNode;
 };
 
 export function TeamProvider({ children }: TeamProviderProps) {
+  const { user } = useAuth();
   const { t } = useTranslation('team');
   const [teams, setTeams] = useState<Team[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    const onCreate = async () => {
+    if (!user) return;
+
+    async function loadTeams() {
       const returnedTeams = await service.getAllTeams();
       returnedTeams.forEach(
         (team) => (team.name = t(`teams.${team.key}.name`)),
@@ -22,10 +26,10 @@ export function TeamProvider({ children }: TeamProviderProps) {
       returnedTeams.sort((t1, t2) => t1.name.localeCompare(t2.name));
       setTeams(returnedTeams);
       setIsLoading(false);
-    };
+    }
 
-    onCreate();
-  }, []);
+    loadTeams();
+  }, [user]);
 
   const value = {
     teams,
